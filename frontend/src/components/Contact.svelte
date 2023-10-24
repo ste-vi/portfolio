0dddfb865b9f6812df825a1f1651e3602786e6b9
stevi-u <trozzato@gmail.com>
2023-10-24 17:18:32 +0300

    added contact page

--- frontend/src/components/Contact.svelte
+++ frontend/src/components/Contact.svelte
@@ -1,11 +1,60 @@
 <script>
+  import { enhance } from "$app/forms";
 
+  const gutHubDomain = "https://github.com";
+  const gitHubUrl = gutHubDomain + "/stevi-u";
+
+  const linkedInDomain = "https://www.linkedin.com/in";
+  const linkedInUrl = linkedInDomain + "/vitalii-stefanchak";
+
+  function openLinkedIn() {
+    window.open(linkedInUrl, "_blank");
+  }
+
+  function openGitHub() {
+    window.open(gitHubUrl, "_blank");
+  }
 </script>
 
 <section class="container" id="contact">
   <div class="content">
-    <div class="contact">
-
+    <div class="links">
+      <div class="social" on:click="{openLinkedIn()}">
+        <div class="icon">
+          <i class="fa-brands fa-linkedin-in fa-beat fa-xl"></i>
+        </div>
+        <div class="social-info">
+          <div class="name">
+            Vitalii Stefanchak
+          </div>
+          <div class="url">
+            <a href="{linkedInUrl}" target="_blank">{linkedInDomain}</a>
+          </div>
+        </div>
+      </div>
+      <div class="social" on:click="{openGitHub()}">
+        <div class="icon">
+          <i class="fa-brands fa-github fa-beat fa-xl"></i>
+        </div>
+        <div class="social-info">
+          <div class="name">
+            Ste Vi
+          </div>
+          <div class="url">
+            <a href="{gitHubUrl}" target="_blank">{gutHubDomain}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="email">
+      <div class="header">Send an e-mail</div>
+      <form action="/" method="POST" use:enhance>
+        <input name="name" placeholder="name">
+        <input name="email" placeholder="your email">
+        <input name="subject" placeholder="subject">
+        <textarea name="message" placeholder="message" />
+        <button type="submit" class="send-btn">submit</button>
+      </form>
     </div>
   </div>
 
@@ -21,17 +70,142 @@
     }
 
     .content {
-        background: rgb(26,18,112);
-        background: linear-gradient(0deg, rgba(26,18,112,1) 21%, rgba(20,16,65,1) 85%);       mix-blend-mode: darken;
+        background: rgb(26, 18, 112);
+        background: linear-gradient(0deg, rgba(26, 18, 112, 1) 21%, rgba(20, 16, 65, 1) 85%);
+        mix-blend-mode: darken;
         justify-content: center;
-        align-items: center;
+        align-items: flex-start;
         height: 100vh;
         width: 100%;
         display: flex;
         flex-direction: row;
+        padding-top: 8rem;
+        gap: 4rem;
     }
 
-    .contact {
-      flex: 1;
+    .email {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        height: 60vh;
+        width: 32%;
+        padding: 2rem 1rem;
+        margin-left: 2rem;
+        border-radius: 1.2rem;
+        background: rgb(90, 87, 238, 0.17);
     }
+
+    .header {
+        font-size: 1.5rem;
+        margin-bottom: 1rem;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        width: 90%;
+        padding: 1rem;
+    }
+
+    input,
+    textarea {
+        background: transparent;
+        color: white;
+        margin-bottom: 3rem;
+        padding: 2px;
+        border: transparent;
+        border-bottom: white 1px solid;
+    }
+
+    textarea {
+        resize: none;
+        height: 16vh;
+    }
+
+    input:focus,
+    textarea:focus {
+        outline: none;
+    }
+
+    input::placeholder,
+    textarea::placeholder {
+        color: white;
+    }
+
+    .send-btn {
+        margin-top: 2rem;
+        margin-left: auto;
+        margin-right: auto;
+        width: 10rem;
+        padding: 0.62rem 2rem;
+        border-radius: 12px;
+        color: white;
+        background-color: #2B28B5;
+        border: 1px solid #2e2ac1;
+        font-size: 1.1rem;
+        cursor: pointer;
+    }
+
+    .send-btn:hover {
+        background-color: #2e2ac5;
+        border: 1px solid #2e2ac1;
+    }
+
+    .send-btn:active {
+        background-color: #1d1a9f;
+    }
+
+    .links {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: start;
+        height: 60vh;
+        width: 30%;
+    }
+
+    .social {
+        width: 100%;
+        background: rgb(90, 87, 238, 0.17);
+        display: flex;
+        flex-direction: row;
+        padding: 2rem 1.4rem;
+        justify-content: flex-start;
+        align-items: start;
+        border-radius: 1.2rem;
+        margin-bottom: 2rem;
+        gap: 1rem;
+        cursor: pointer;
+    }
+
+    .social:hover {
+        background: rgb(90, 87, 238, 0.20);
+        border: 1px solid rgb(90, 87, 238, 0.2);
+    }
+
+    .social:active {
+        background-color: rgb(90, 87, 238, 0.50);
+        border: 2px solid rgb(90, 87, 238, 0.50);
+    }
+
+    .icon {
+        align-self: center;
+        padding: 1rem;
+        border-radius: 16px;
+        width: fit-content;
+        height: fit-content;
+        background-color: #1d1559;
+    }
+
+    .name {
+        padding-bottom: 0.8rem;
+    }
+
+    a {
+        color: #628EFF;
+    }
+
+
 </style>
--- frontend/src/components/Work.svelte
+++ frontend/src/components/Work.svelte
@@ -1,25 +1,14 @@
 <script>
   let projects = [
     {
-      "name": "Project One",
-      "stack": ["Java 17", "Spring", "PostgreSQL", "Kubernetes"],
-      "description": "Desc",
-      "responsibilities": "Worked closely with the development team under the Agile (Scrum) methodology\n" +
-        "to deliver high-quality software.\n" +
-        "Implemented new business requirements by developing new modules from\n" +
-        "scratch based on the Spring framework.\n" +
-        "Participated in support activities, investigated and resolved issues, and\n" +
-        "implemented new technical solutions"
-    },
-    {
       "name": "Project Two",
       "stack": ["Java 17", "Spring", "PostgreSQL", "Kubernetes", "GraphQL", "Docker", "Microservices", "AWS"],
       "description": "The application is a big platform that helps large and middle-size companies/factories control the quality of products their produce and manage relationships between suppliers and consumers.",
-      "responsibilities": "Worked closely with the development team under the Agile (Scrum) methodology\n" +
+      "responsibilities": "- Worked closely with the development team under the Agile (Scrum) methodology\n" +
         "to deliver high-quality software.\n" +
-        "Implemented new business requirements by developing new modules from\n" +
+        "- Implemented new business requirements by developing new modules from\n" +
         "scratch based on the Spring framework.\n" +
-        "Participated in support activities, investigated and resolved issues, and\n" +
+        "- Participated in support activities, investigated and resolved issues, and\n" +
         "implemented new technical solutions"
     },
     {
@@ -36,114 +25,47 @@
         "- Took part in continuous releases of projects on time, meeting client expectations.\n" +
         "- Refactored source code which helped in a project's development speed and\n" +
         "efficiency"
-    },
-    {
-      "name": "Project Four",
-      "stack": ["Java 17", "Spring", "PostgreSQL", "Kubernetes", "GraphQL", "Docker", "Microservices", "AWS"],
-      "description": "The application is a big platform that helps large and middle-size companies/factories control the quality of products their produce and manage relationships between suppliers and consumers.",
-      "responsibilities": "Worked closely with the development team under the Agile (Scrum) methodology\n" +
-        "to deliver high-quality software.\n" +
-        "Implemented new business requirements by developing new modules from\n" +
-        "scratch based on the Spring framework.\n" +
-        "Participated in support activities, investigated and resolved issues, and\n" +
-        "implemented new technical solutions"
     }
   ];
 
-
-  let scrollPosition = 0;
-  let cardWidth = 200;
-  let numberOfCards = projects.length;
-  let currentCard = 0;
-
-  let currentIndex = 0;
-
-  currentIndex = Math.floor(numberOfCards / 2);
-  console.log(currentIndex);
-
-
-  function scrollLeft() {
-    currentIndex--;
-    console.log(currentIndex);
-
-
-    if (currentIndex > 0) {
-      scrollPosition = scrollPosition + cardWidth;
-    } else {
-      currentIndex = numberOfCards - 1;
-      scrollPosition = 0 - (cardWidth * currentIndex);
-    }
-
-  }
-
-  function scrollRight() {
-    currentIndex++;
-    if (currentIndex == currentIndex % numberOfCards) {
-
-    }
-    console.log(currentIndex);
-
-    //currentIndex = (currentIndex + 1) % numberOfCards;
-  }
 </script>
 
 <section class="container" id="work">
   <div class="content">
     <div class="title">
-      Projects
+      Recent Projects
     </div>
 
-    <div class="cards-content">
-
-      <div class="left" on:click={scrollLeft} role="button">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="40" viewBox="0 0 26 46" fill="none">
-          <path
-            d="M0.87868 20.8787C-0.292893 22.0503 -0.292893 23.9497 0.87868 25.1213L19.9706 44.2132C21.1421 45.3848 23.0416 45.3848 24.2132 44.2132C25.3848 43.0416 25.3848 41.1421 24.2132 39.9706L7.24264 23L24.2132 6.02944C25.3848 4.85786 25.3848 2.95837 24.2132 1.7868C23.0416 0.615223 21.1421 0.615223 19.9706 1.7868L0.87868 20.8787ZM3 26H4V20H3V26Z"
-            fill="white" />
-        </svg>
-      </div>
-
-      <div class="cards">
-        {#each projects as project, index (index)}
-          <div class="card" style="transform: translateX({scrollPosition}px);" bind:clientWidth={cardWidth}>
-            {index}
-
-            <div class="image">
-              <img src="/test.png" alt="image" />
-            </div>
-            <div class="name">
-              {project.name}
-            </div>
-            <div class="stack">
-              {#each project.stack as item}
-                <div class="stack-item">
-                  {item}
-                </div>
-              {/each}
-            </div>
-            <div class="description">
-              <b>Description:</b><br>
-              <p>
-                {project.description}
-              </p>
-            </div>
-            <div class="responsibilities">
-              <b>Responsibilities:</b><br>
-              <p>
-                {project.responsibilities}
-              </p>
-            </div>
+    <div class="cards">
+      {#each projects as project}
+        <div class="card">
+          <div class="image">
+            <img src="/test.png" alt="image" />
           </div>
-        {/each}
-      </div>
-
-      <div class="right" on:click={scrollRight} role="button">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="40" viewBox="0 0 26 46" fill="none">
-          <path
-            d="M25.1213 25.1213C26.2929 23.9497 26.2929 22.0503 25.1213 20.8787L6.02944 1.7868C4.85786 0.615223 2.95837 0.615223 1.7868 1.7868C0.615223 2.95837 0.615223 4.85786 1.7868 6.02944L18.7574 23L1.7868 39.9706C0.615223 41.1421 0.615223 43.0416 1.7868 44.2132C2.95837 45.3848 4.85786 45.3848 6.02944 44.2132L25.1213 25.1213ZM22 26H23V20H22V26Z"
-            fill="white" />
-        </svg>
-      </div>
+          <div class="name">
+            {project.name}
+          </div>
+          <div class="stack">
+            {#each project.stack as item}
+              <div class="stack-item">
+                {item}
+              </div>
+            {/each}
+          </div>
+          <div class="description">
+            <b>Description:</b><br>
+            <p>
+              {project.description}
+            </p>
+          </div>
+          <div class="responsibilities">
+            <b>Responsibilities:</b><br>
+            <p>
+              {project.responsibilities}
+            </p>
+          </div>
+        </div>
+      {/each}
     </div>
   </div>
 
@@ -177,36 +99,24 @@
         padding-top: 3rem;
     }
 
-    .cards-content {
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        justify-content: center;
-        margin-top: 2rem;
-        margin-left: 2rem;
-        margin-right: 2rem;
-        align-items: center;
-        gap: 1.8rem;
-    }
-
     .cards {
         display: flex;
         flex-direction: row;
         gap: 1rem;
-        width: 80%;
-
+        width: 86%;
         justify-content: center;
+        align-items: start;
+        margin-top: 2rem;
+        margin-left: 2rem;
+        margin-right: 2rem;
 
         overflow-x: auto;
         flex-wrap: nowrap;
 
         -webkit-overflow-scrolling: touch;
-
         scroll-behavior: smooth;
 
-        &::-webkit-scrollbar {
-            display: none;
-        }
+        padding-bottom: 2rem;
     }
 
     .card {
@@ -215,12 +125,10 @@
         min-height: 65vh;
         max-width: 500px;
         min-width: 400px;
-
         border-radius: 7px;
         padding: 1.2rem;
-        flex: 0 0 auto;
+        flex: 1 0 auto;
         cursor: pointer;
-        display: inline-block;
         transition: transform 0.5s ease-in-out;
     }
 
@@ -269,22 +177,13 @@
     .description {
         padding-top: 1rem;
         padding-bottom: 0.1rem;
-        font-size: 0.74rem;
+        font-size: 0.84rem;
         white-space: pre-line;
     }
 
     .responsibilities {
         white-space: pre-line;
-        font-size: 0.74rem;
-    }
-
-    svg {
-        cursor: pointer;
-    }
-
-    path:hover {
-        fill: #364be8 !important;
-        cursor: pointer;
+        font-size: 0.84rem;
     }
 
     @media only screen and (max-width: 600px) and (max-height: 670px) {
